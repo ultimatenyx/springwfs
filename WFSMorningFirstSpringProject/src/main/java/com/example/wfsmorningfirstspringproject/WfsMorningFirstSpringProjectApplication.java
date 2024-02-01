6c76811ced7b15b4d6d08eff53708217c0cfc42c
Arcanyx <nikhilchaughule@gmail.com>
2024-02-01 23:10:45 +0530

    Day One

---
+++ WFSMorningFirstSpringProject/src/main/java/com/example/wfsmorningfirstspringproject/WfsMorningFirstSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wfsmorningfirstspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfsMorningFirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfsMorningFirstSpringProjectApplication.class, args);
+	}
+
+}
---
+++ WFSMorningFirstSpringProject/src/test/java/com/example/wfsmorningfirstspringproject/WfsMorningFirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wfsmorningfirstspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfsMorningFirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
